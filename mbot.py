376efe2f84ece22c486612beb55fd13cf234ec32
l0qii <github@apathyhouse.com>
2018-03-13 00:36:33 -0400

    Initial commit

---
+++ mbot.py
@@ -0,0 +1,41 @@
+from twitchio import commands as tcommands
+import os
+from mtgsdk import Card
+from mtgsdk import Set
+from mtgsdk import Type
+from mtgsdk import Supertype
+from mtgsdk import Subtype
+from mtgsdk import Changelog
+
+class Mbot(tcommands.TwitchBot):
+    """Create our IRC Twitch Bot.
+    api_token is optional, but without it, you will not be able to make certain calls to the API."""
+
+    def __init__(self):
+        super().__init__(prefix=['!', '?'], token=os.environ['IRC_TOKEN'], api_token='API_TOKEN', client_id='CLIENT_ID',
+                         nick='onulet', initial_channels=['#kevin_spicy'])
+
+    async def event_ready(self):
+        """Event called when the bot is ready to go!"""
+        print('READY!')
+
+    # async def event_message(self, message):
+    #     """Event called when a message is sent to a channel you are in."""
+    #     if message.content == 'Hello':
+    #         await message.send('World!')
+
+    @tcommands.twitch_command(aliases=['card'])
+    async def card_lookup(self, ctx):
+        response = 'card not found {}'.format(ctx.content[6:])
+        cards = Card.where(name=ctx.content[6:]).all()
+        if cards:
+            card = cards[0]
+            if 'creature' in card.type.lower():
+                response = '\\\\{}// {} {}/{}, {} -- {}'.format(card.name, card.type, card.power, card.toughness, card.mana_cost, card.text)
+            else:
+                response = '\\\\{}// {}, {} -- {}'.format(card.name, card.type, card.mana_cost, card.text)
+        await ctx.send(response)
+
+bot = Mbot()
+bot.run()
+
